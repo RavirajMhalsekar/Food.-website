59228dc7c02e857577e5d8e2654cc5b468721a21
RavirajMhalsekar <ravirajmhalsekar@gmail.com>
2021-07-17 14:14:39 +0530

    project complete

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function togglemenu(){
+    const menutoggle = document.querySelector('.menutoggle');
+    menutoggle.classList.toggle('active');
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+}
